24e57e58c2a08e0722c38d4ce30ee2d0daea304e
Cody Chhay <menghengchhay@gmail.com>
2019-11-07 21:09:56 +0000

    implement js .bind

---
+++ index.js
@@ -0,0 +1,9 @@
+//Implement .bind()
+
+Function.prototype.bind = function(whoIsCallingMe, ...args){
+  console.log(args)
+  const self = this;
+  return function(){
+    return self.apply(whoIsCallingMe, args);
+  };
+}
